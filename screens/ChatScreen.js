9bdf7add29b89a53b7a1e806bfff39b6989e3ceb
Vince Ding <vince.ding@gmail.com>
2022-07-16 14:28:50 +0800

    Version 1: Boiler plate

---
+++ screens/ChatScreen.js
@@ -0,0 +1,59 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+import { GiftedChat } from "react-native-gifted-chat";
+import firebase from "../database/firebaseDB";
+const auth = firebase.auth();
+const db = firebase.firestore().collection("messages");
+
+export default function ChatScreen() {
+  const [messages, setMessages] = useState([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribe = db
+      .orderBy("createdAt", "desc")
+      .onSnapshot((collectionSnapshot) => {
+        const messages = collectionSnapshot.docs.map((doc) => {
+          const date = doc.data().createdAt.toDate();
+          const newDoc = { ...doc.data(), createdAt: date };
+          return newDoc;
+        });
+        setMessages(messages);
+      });
+
+    auth.onAuthStateChanged((user) => {
+      if (user) navigation.navigate("Chat", { id: user.id, email: user.email });
+      else navigation.navigate("Login");
+    });
+
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={logout}>
+          <MaterialCommunityIcons name="logout" size={30} color="grey" />
+        </TouchableOpacity>
+      ),
+    });
+    return unsubscribe;
+  }, []);
+
+  const logout = () => auth.signOut();
+
+  function sendMessages(newMessages) {
+    console.log(newMessages);
+    db.add(newMessages[0]);
+  }
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={sendMessages}
+      listViewProps={{ style: { backgroundColor: "grey" } }}
+      user={{ _id: auth.currentUser?.uid, name: auth.currentUser?.email }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/LoginScreen.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import firebase from "../database/firebaseDB";
+const auth = firebase.auth();
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState("");
+
+  async function login() {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+      setErrorText(error.message);
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Chat App</Text>
+
+      <Text style={styles.fieldTitle}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <Text style={styles.fieldTitle}>Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <TouchableOpacity style={styles.loginButton} onPress={login}>
+        <Text style={styles.buttonText}>Log In</Text>
+      </TouchableOpacity>
+      <Text style={styles.errorText}>{errorText}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  errorText: {
+    color: "red",
+    marginVertical: 20,
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  fieldTitle: {
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  input: {
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 24,
+    padding: 4,
+    height: 36,
+    fontSize: 18,
+    backgroundColor: "white",
+  },
+  loginButton: {
+    backgroundColor: "blue",
+    width: 120,
+    alignItems: "center",
+    padding: 18,
+    marginTop: 12,
+    marginBottom: 36,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
